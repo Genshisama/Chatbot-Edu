65de749dcec1de8f9bf5f4711b2470b5adafc4a8
Genshisama <s.syaqir22@gmail.com>
2023-12-03 16:09:10 +0800

    latest

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, jsonify
 import time
 import openai
+import json
 
-api_key = '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'
+with open('config.json') as config_file:
+    config_data = json.load(config_file)
+
+api_key = config_data.get('api_key')
 last_request_time = time.time()
 
 app = Flask(__name__)
@@ -18,6 +22,9 @@ def chatbot_interface():
 def send_message():
     user_input = request.json['user_input']
     
+    if not user_input:
+        return jsonify({'response': "Sorry but it seems like you forgot to input your inquiry. Please try again."})
+        
     chatbot_response = rate_limited_assistant(f"You: {user_input}\nAssistant:")
     
     time.sleep(1)
@@ -33,7 +40,7 @@ def rate_limited_assistant(prompt):
     elapsed_time = time.time() - last_request_time
     
     if elapsed_time < 5:
-        time.sleep(5-elapsed_time)
+        time.sleep(5 - elapsed_time)
         
     last_request_time = time.time()
     
@@ -41,20 +48,42 @@ def rate_limited_assistant(prompt):
     
     response_text = ""
 
+    messages = [
+        {
+            "role": "system", "content": """
+                You are a historical facts expert, well-versed in various historical events and figures. 
+                You can provide detailed and accurate answers to history-related questions. 
+                If the query is related to a different subject (e.g., math, science, literature), clearly state that you are focused on history and unable to assist with inquiries outside this field. 
+                If the question pertains to an unrelated topic, kindly express that it falls outside your field of expertise. 
+                If the user asks who or what you are, say you are a historical expert.
+                Answer briefly unless asked for explanation.
+                Greet user in brief manner.
+            """
+        },
+        {
+            "role": "user", "content": """
+                You are a user seeking information from a historical facts expert. 
+                Feel free to ask any history-related questions, and the expert will provide you with accurate information.
+            """
+        },
+        {"role": "assistant", "content": "Yes, I understand. Please go ahead and ask any historical question, and I'll do my best to provide you with detailed and accurate information."}
+    ]
+    
     try:
         while True:
+            message = prompt
+            if message:
+                messages.append(
+                    {"role": "user", "content": message},
+                )
             response = openai.chat.completions.create(
                 model="gpt-3.5-turbo",
-                messages = [
-                    {
-                        "role": "system", "content": """
-                            You are a game developer. You generate python scripts for games based on what people describe to you.
-                            """
-                    },
-                    {"role": "user", "content": "Hi, I need you to help me create a game in Python. I will describe the game to you and you will give me back a script for it. Be sure to always include code for a GUI that appears for me to play the game on. Is that ok?"},
-                    {"role": "assistant", "content": "Yes of course. Please provide me with some details on this game!"},
-                ]
-
+                messages=messages,
+                temperature=0.7,
+                max_tokens=150,
+                top_p=1.0,
+                frequency_penalty=0,
+                presence_penalty=0
             )
             
             current_response = response.choices[0].message.content
@@ -64,12 +93,15 @@ def rate_limited_assistant(prompt):
             if '.' in current_response or len(response_text.split()) >= 50:
                 break
             
-            prompt = current_response
+            # Update the prompt for the next iteration
+            messages[-1]["content"] = f"You: {current_response}\nAssistant:"
 
     except Exception as e:
         print(f"Error during API request: {e}")
         
     return response_text
+
+
 
 
 if __name__ == '__main__':
